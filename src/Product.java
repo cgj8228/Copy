272ca219142956b95bca2a9247b7320dce88a787
최길중 <choil8228@gmail.com>
2026-01-24 00:42:05 +0900

    레벨1 완료

--- src/Product.java
+++ src/Product.java
@@ -10,4 +10,12 @@ public class Product {
         this.description = description;
         this.count = count;
     }
+
+    public String getMySimpleInfo(){
+        return this.name + " | " + this.price + " | " + this.description;
+    }
+
+    public String getMyInfo(){
+        return this.name + " | " + this.price + " | " + this.description+ " | " +this.count;
+    }
 }
