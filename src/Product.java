951668531f31055ee51d1006663256c83eaeb7c5
최길중 <choil8228@gmail.com>
2026-01-23 23:36:19 +0900

    first commit

---
+++ src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    private String name;
+    private Integer price;
+    private String description;
+    private Integer count;
+
+    public Product(String name, Integer price, String description, Integer count){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.count = count;
+    }
+}
